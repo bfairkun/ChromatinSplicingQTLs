ea425ea0d4eafe82bf3fff9b1e4a6c44222f5e79
Benjmain Fair <bfair.kun@gmail.com>
2022-12-15 13:15:54 -0600

    add sm processing nb

--- code/scripts/Collapse_Juncfiles.R
+++ code/scripts/Collapse_Juncfiles.R
@@ -11,7 +11,7 @@
 #Use hard coded arguments in interactive R session, else use command line args
 if(interactive()){
     args <- scan(text=
-                 "my extended list", what='character')
+                 "SmallMolecule/FullSpliceSiteAnnotations/ALL_SAMPLES.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_3160_LCL_chRNA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_3160_LCL_chRNA_2.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_3160_LCL_chRNA_3.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_100_LCL_chRNA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_100_LCL_chRNA_2.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_100_LCL_chRNA_3.junc SmallMolecule/leafcutter/juncfiles/autosomes/DMSO_NA_LCL_chRNA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/DMSO_NA_LCL_chRNA_2.junc SmallMolecule/leafcutter/juncfiles/autosomes/DMSO_NA_LCL_chRNA_3.junc SmallMolecule/leafcutter/juncfiles/autosomes/DMSO_NA_LCL_polyA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/DMSO_NA_LCL_polyA_2.junc SmallMolecule/leafcutter/juncfiles/autosomes/DMSO_NA_LCL_polyA_3.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_10000_LCL_polyA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_3160_LCL_polyA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_1000_LCL_polyA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_316_LCL_polyA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_100_LCL_polyA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_31.6_LCL_polyA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_10_LCL_polyA_1.junc SmallMolecule/leafcutter/juncfiles/autosomes/Risdiplam_3.16_LCL_polyA_1.junc", what='character')
 } else{
     args <- commandArgs(trailingOnly=TRUE)
 }
@@ -29,8 +29,12 @@ dat %>%
     separate(V11, into=c("bSize1", "bSize2"), sep=",", convert=T, remove=F) %>%
     mutate(JuncStart = V2 + bSize1) %>%
     mutate(JuncEnd = V3 - bSize2) %>%
-    distinct(JuncStart, JuncEnd, .keep_all=T) %>%
-    select(-c("bSize1", "bSize2", "JuncStart", "JuncEnd")) %>%
+    group_by(V1, JuncStart, JuncEnd, V6) %>%
+    mutate(SumCount = sum(V5)) %>%
+    ungroup() %>%
+    distinct(V1, JuncStart, JuncEnd, .keep_all=T) %>%
+    dplyr::select(-c("bSize1", "bSize2", "JuncStart", "JuncEnd", "V5")) %>%
+    dplyr::select(V1:V4, SumCount, everything()) %>%
     arrange(V1, V2, V3) %>%
     write_tsv(fout, col_names=F)
 
